fa8eb8f57c687e61b8ed194f1ccd970f26624102
andreterto-dev <andreterto.dev@gmail.com>
2024-08-03 10:55:44 -0300

    docs: add images for README

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,11 +1,10 @@
-// resources/js/Components/ApplicationLogo.jsx
 export default function ApplicationLogo(props) {
     return (
         <img
             {...props}
             src="/images/logo.png"
             alt="Application Logo"
-            style={{ height: '45px', width: 'auto' }} // Ajuste o tamanho conforme necessário
+            style={{ height: '45px', width: 'auto' }}
         />
     );
 }
